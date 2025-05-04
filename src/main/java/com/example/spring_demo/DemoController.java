3d474e0d21dd6b933e2ffd6e127b292e78f29929
hongli336 <hongli336@gmail.com>
2025-05-03 21:17:55 -0500

    update Jenkinsfile to use env for database password

--- src/main/java/com/example/spring_demo/DemoController.java
+++ src/main/java/com/example/spring_demo/DemoController.java
@@ -8,7 +8,8 @@ public class DemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, world!";
+        //return "Hello, world!";
+        return new HelloResponse("Hello, world!");
     }
 
 }
