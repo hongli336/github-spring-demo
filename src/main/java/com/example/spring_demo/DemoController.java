c20b4468fce8a028e7c0008c55eaae5ea94fbea0
hongli336 <hongli336@gmail.com>
2025-05-03 22:23:18 -0500

    add post api function

--- src/main/java/com/example/spring_demo/DemoController.java
+++ src/main/java/com/example/spring_demo/DemoController.java
@@ -2,6 +2,9 @@ package com.example.spring_demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class DemoController {
