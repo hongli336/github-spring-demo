328ab7def9ef85b3b3a404a04edb72a45d78dada
hongli336 <hongli336@gmail.com>
2025-05-07 22:42:07 -0500

    Service interface / impl classes

--- src/main/java/com/example/spring_demo/service/CategoryService.java
+++ src/main/java/com/example/spring_demo/service/CategoryService.java
@@ -2,6 +2,9 @@ package com.example.spring_demo.service;
 
 import com.example.spring_demo.model.Category;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface CategoryService {
     List<Category> getAllCategories();
     void createCategory(Category category);
