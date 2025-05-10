001ff936ba5540e0c8e624fb00fc08bfd50677f7
hongli336 <hongli336@gmail.com>
2025-05-09 22:26:25 -0500

    deletecategory

--- src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
+++ src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
@@ -27,8 +27,8 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId)
-                        .findFirst().get());
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                        .findFirst().get();
         categories.remove(category);
         return "Category with CategoryId: " + categroyId;
     }
