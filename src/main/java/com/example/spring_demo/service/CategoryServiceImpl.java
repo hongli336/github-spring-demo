1b55e4146d2ea3ac7d86ada68222c1111a25c105
hongli336 <hongli336@gmail.com>
2025-05-10 21:51:15 -0500

    delete category exception handle

--- src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
+++ src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
