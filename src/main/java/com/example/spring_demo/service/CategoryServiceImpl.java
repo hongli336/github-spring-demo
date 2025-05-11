55d2336b0ac84f5662a223f0961ca4546c113ac7
hongli336 <hongli336@gmail.com>
2025-05-10 21:48:31 -0500

    delete category exception handle

--- src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
+++ src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
@@ -28,7 +28,7 @@ public class CategoryServiceImpl implements CategoryService {
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
-                        .findFirst().orElse(null);
+                        .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         if (category == null)
             return "Category not found";
