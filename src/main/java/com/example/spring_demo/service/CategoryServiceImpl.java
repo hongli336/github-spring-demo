af7c47044ec0cdcabe7863d9f44adaea4c510fd8
hongli336 <hongli336@gmail.com>
2025-05-21 22:51:59 -0500

    JPA Update CategoryService

--- src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
+++ src/main/java/com/example/spring_demo/service/CategoryServiceImpl.java
@@ -30,7 +30,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
+        //category.setCategoryId(nextId++);
         //categories.add(category);
         categoryRepository.save(category);
     }
