4cd4fe0fa27516e213f5c3a97094e0131165b3c7
hongli336 <hongli336@gmail.com>
2025-06-29 08:21:26 -0500

    modelmapper

--- src/main/java/com/example/spring_demo/payload/CategoryResponse.java
+++ src/main/java/com/example/spring_demo/payload/CategoryResponse.java
@@ -6,6 +6,21 @@ import lombok.NoArgsConstructor;   // Generates a no-argument constructor
 
 import java.util.List;
 
+/*
+Future-Proofing
+Maybe today you're only returning a list of categories, but in the future you might want to include:
+
+Pagination info
+
+A summary count
+
+Server timestamp
+
+Warnings or user messages
+
+With a CategoryResponse, you don’t need to change the controller or client code — you just add new fields to the wrapper class.
+ */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
